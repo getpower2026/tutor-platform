e971149106b72b0fef04d70f4505f40f576e0425
abcaaaa <tantriswang@gmail.com>
2026-06-23 10:58:57 +0800

    Add Vercel Blob domain to Next.js image config

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ const nextConfig: NextConfig = {
   serverExternalPackages: ["@prisma/client", "bcryptjs"],
   images: {
     domains: ["lh3.googleusercontent.com", "avatars.githubusercontent.com"],
+    remotePatterns: [
+      { protocol: "https", hostname: "*.public.blob.vercel-storage.com" },
+    ],
   },
 };
 
