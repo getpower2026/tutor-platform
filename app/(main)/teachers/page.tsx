2aeecafd21286c446b29808f5650e3cbb18804d0
abcaaaa <tantriswang@gmail.com>
2026-06-24 13:21:00 +0800

    老師列表改為動態渲染，新老師即時顯示

--- app/(main)/teachers/page.tsx
+++ app/(main)/teachers/page.tsx
@@ -13,6 +13,8 @@ export const metadata: Metadata = {
   },
 };
 
+export const dynamic = "force-dynamic";
+
 export default async function TeachersPage() {
   const teachers = await prisma.teacherProfile.findMany({
     include: {
